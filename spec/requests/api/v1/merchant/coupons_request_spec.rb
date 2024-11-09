3259d155a58bbbfdbc37d4412f5274d62f0e7f12
devklynch <devklynch@gmail.com>
2024-11-09 11:01:45 -0700

    tests for updated endpoints

--- spec/requests/api/v1/merchant/coupons_request_spec.rb
+++ spec/requests/api/v1/merchant/coupons_request_spec.rb
@@ -6,9 +6,11 @@ RSpec.describe "Merchant coupons endpoints" do
         @merchant_1 = FactoryBot.create(:merchant)
         @merchant_3 = FactoryBot.create(:merchant)
       
-        @coupon_1 = FactoryBot.create(:coupon, merchant_id: @merchant_1.id)
+        @coupon_1 = FactoryBot.create(:coupon, active: true, merchant_id: @merchant_1.id)
         @coupon_2 = FactoryBot.create(:coupon, merchant_id: @merchant_2.id)
         @coupon_3 = FactoryBot.create(:coupon, merchant_id: @merchant_2.id)
+        @coupon_4 = FactoryBot.create(:coupon, active: true, merchant_id: @merchant_1.id)
+        @coupon_5 = FactoryBot.create(:coupon, active: false, merchant_id: @merchant_1.id)
 
     end
 
@@ -41,6 +43,29 @@ RSpec.describe "Merchant coupons endpoints" do
             expect(response).to have_http_status(:not_found)
             expect(json_response[:message]).to eq("Your query could not be completed")
             expect(json_response[:errors]).to eq(["Couldn't find Merchant with 'id'=999999"])
+        end
+
+        it "can give a merchant's only active invoices if queried" do
+            get "/api/v1/merchants/#{@merchant_1.id}/coupons?active=true"
+
+            coupons = JSON.parse(response.body, symbolize_names: true)
+            #binding.pry
+            expect(response.status).to eq(200)
+            expect(coupons[:data].count).to eq(2)
+            expect(coupons[:data][0][:id]).to eq(@coupon_1.id.to_s)
+            expect(coupons[:data][1][:id]).to eq(@coupon_4.id.to_s)
+
+        end
+
+        it "can give a merchant's only inactive invoices if queried" do
+            get "/api/v1/merchants/#{@merchant_1.id}/coupons?active=false"
+
+            coupons = JSON.parse(response.body, symbolize_names: true)
+            #binding.pry
+            expect(response.status).to eq(200)
+            expect(coupons[:data].count).to eq(1)
+            expect(coupons[:data][0][:id]).to eq(@coupon_5.id.to_s)
+
         end
     end
 
